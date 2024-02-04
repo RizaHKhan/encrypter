2bbde556699492bbb513a9085c33fd6e1c7464f4
Riza Khan <khanriza@gmail.com>
2024-02-04 13:25:56 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+	"os"
+)
+
+func encrypt(fileIn, fileOut string, key []byte) error {
+	// Read the plaintext file
+	plaintext, err := os.ReadFile(fileIn)
+	if err != nil {
+		return err
+	}
+
+	// Generate a random nonce
+	nonce := make([]byte, 12)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return err
+	}
+
+	// Create a new AES cipher block using the provided key
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	// Create a new GCM cipher
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	// Encrypt the plaintext using AES-GCM
+	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+
+	// Write the nonce and ciphertext to the output file
+	ciphertext = append(nonce, ciphertext...)
+	if err := os.WriteFile(fileOut, ciphertext, 0644); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func decrypt(fileIn, fileOut string, key []byte) error {
+	// Read the encrypted file
+	ciphertext, err := os.ReadFile(fileIn)
+	if err != nil {
+		return err
+	}
+
+	// Extract the nonce from the ciphertext
+	nonce := ciphertext[:12]
+	ciphertext = ciphertext[12:]
+
+	// Create a new AES cipher block using the provided key
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	// Create a new GCM cipher
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	// Decrypt the ciphertext using AES-GCM
+	plaintext, err := aesgcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return err
+	}
+
+	// Write the decrypted plaintext to the output file
+	if err := os.WriteFile(fileOut, plaintext, 0644); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	key := []byte("your-32-byte-aes") // Replace with your key
+
+	// Encrypt a file
+	if err := encrypt("./plaintext.txt", "./encrypted.bin", key); err != nil {
+		panic(err)
+	}
+	println("Encryption successful")
+
+	// Decrypt the file
+	if err := decrypt("./encrypted.bin", "./decrypted.txt", key); err != nil {
+		panic(err)
+	}
+	println("Decryption successful")
+}
